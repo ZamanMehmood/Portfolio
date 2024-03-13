95c60695ed66cd8b6eb8da4e1569ff31a7bc2b88
zaman <zaman342003@gmail.com>
2024-03-13 22:22:22 +0000

    download resume

--- app/page.js
+++ app/page.js
@@ -10,6 +10,7 @@ import bookstoreApi from "../public/bookstoreApi.png";
 import crypto from "../public/crypto.png";
 import fullstack02 from "../public/isoftStudios.png";
 import "../app/app.css"
+import func from "../public/index"
 
 
 
@@ -28,9 +29,11 @@ export default function Home() {
               <BsFillMoonStarsFill className="dark:text-white" />
             </li>
             <li className="bg-gradient-to-r from-cyan-500 to-blue-500 py-2 px-4 text-white ml-5 rounded-md">
-              <a href="https://github.com/ZamanMehmood" target="blank">
+                <a href="RESUME.pdf" download>
                 RESUME
+
               </a>
+              {/* href="variable_name" download */}
             </li>
           </ul>
         </nav>
---
+++ public/index.jsx
@@ -0,0 +1,11 @@
+const Act = () =>{
+    const func = () =>{
+        console.log("funct")
+    }
+    return (
+        <h2>hello world
+        </h2>
+    )
+}
+
+export default Act;
